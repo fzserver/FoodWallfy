91868dab24b6211bbb5072e83eca46f932165d93
Frazile Inc <Frazile.com@gmail.com>
2020-07-07 05:49:14 +0530

    Bugs Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 // import 'package:foodwallfy/constants/frazile.dart';
@@ -29,10 +29,10 @@ void main() {
   // This is only to be used for confirming that reports are being
   // submitted as expected. It is not intended to be used for everyday
   // development.
-  Crashlytics.instance.enableInDevMode = true;
+  // Crashlytics.instance.enableInDevMode = true;
 
   // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
+  // FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(ConfigPage());
 }
@@ -41,3 +41,4 @@ void main() {
 //   DBManager db = DBManager.internal();
 //   await db.initDb();
 // }
+
