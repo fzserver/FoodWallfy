e6e3aabec98d3d777eb85ad5a165ff82f656ddc6
Frazile Inc <Frazile.com@gmail.com>
2020-07-16 07:24:29 +0530

    Bugs Fixed

--- lib/pages/donate/Donate.dart
+++ lib/pages/donate/Donate.dart
@@ -317,68 +317,79 @@ class _DonateState extends State<Donate> with SingleTickerProviderStateMixin {
                     ),
                   ),
                 ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height * .11,
-                  left: 10.0,
-                  right: 10.0,
-                  bottom: 0.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      FittedBox(
-                        child: DonateItem(
-                          image: 'comedy',
-                          gradient: FzGradients.redSexyGradient,
-                          title: 'Donate \$1 by RazorPay',
-                          color: FzGradients.redSexyGradient.colors[0],
-                          animation: animation,
-                          paymentMode: 'RazorPay',
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      FittedBox(
-                        child: DonateItem(
-                          image: 'action',
-                          gradient: FzGradients.greenSexyGradient,
-                          title: 'Buy me a beer by RazorPay for \$10',
-                          color: FzGradients.greenSexyGradient.colors[0],
-                          animation: animation,
-                          paymentMode: 'RazorPay',
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      FittedBox(
-                        child: DonateItem(
-                          image: 'romance',
-                          gradient: FzGradients.pinkRedGradient,
-                          title: 'Buy me a iPhone by IAP for \$200',
-                          color: FzGradients.pinkRedGradient.colors[0],
-                          animation: animation,
-                          paymentMode: 'IAP',
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                      FittedBox(
-                        child: DonateItem(
-                          image: 'horror',
-                          gradient: FzGradients.skyBlueGradient,
-                          title: 'Buy me a MacBook by IAP for \$2000',
-                          color: FzGradients.skyBlueGradient.colors[0],
-                          animation: animation,
-                          paymentMode: 'IAP',
-                        ),
-                      ),
-                    ],
+                Center(
+                  child: Text(
+                    'Coming Soon',
+                    style: TextStyle(
+                      color: Colors.indigo,
+                      fontSize: 24.0,
+                      letterSpacing: 2.0,
+                      wordSpacing: 2.5,
+                    ),
                   ),
                 ),
+                // Positioned(
+                //   top: MediaQuery.of(context).size.height * .11,
+                //   left: 10.0,
+                //   right: 10.0,
+                //   bottom: 0.0,
+                //   child: Column(
+                //     mainAxisAlignment: MainAxisAlignment.start,
+                //     crossAxisAlignment: CrossAxisAlignment.center,
+                //     mainAxisSize: MainAxisSize.min,
+                //     children: [
+                //       FittedBox(
+                //         child: DonateItem(
+                //           image: 'comedy',
+                //           gradient: FzGradients.redSexyGradient,
+                //           title: 'Donate \$1 by RazorPay',
+                //           color: FzGradients.redSexyGradient.colors[0],
+                //           animation: animation,
+                //           paymentMode: 'RazorPay',
+                //         ),
+                //       ),
+                //       SizedBox(
+                //         height: 10.0,
+                //       ),
+                //       FittedBox(
+                //         child: DonateItem(
+                //           image: 'action',
+                //           gradient: FzGradients.greenSexyGradient,
+                //           title: 'Buy me a beer by RazorPay for \$10',
+                //           color: FzGradients.greenSexyGradient.colors[0],
+                //           animation: animation,
+                //           paymentMode: 'RazorPay',
+                //         ),
+                //       ),
+                //       SizedBox(
+                //         height: 10.0,
+                //       ),
+                //       FittedBox(
+                //         child: DonateItem(
+                //           image: 'romance',
+                //           gradient: FzGradients.pinkRedGradient,
+                //           title: 'Buy me a iPhone by IAP for \$200',
+                //           color: FzGradients.pinkRedGradient.colors[0],
+                //           animation: animation,
+                //           paymentMode: 'IAP',
+                //         ),
+                //       ),
+                //       SizedBox(
+                //         height: 10.0,
+                //       ),
+                //       FittedBox(
+                //         child: DonateItem(
+                //           image: 'horror',
+                //           gradient: FzGradients.skyBlueGradient,
+                //           title: 'Buy me a MacBook by IAP for \$2000',
+                //           color: FzGradients.skyBlueGradient.colors[0],
+                //           animation: animation,
+                //           paymentMode: 'IAP',
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
                 // Center(
                 //   child: Column(
                 //     children: [
